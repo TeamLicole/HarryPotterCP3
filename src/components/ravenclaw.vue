981fd24aed06e65ffd1eda5aabcd03e9b2b2a432
Lindsey Woolf <woolflindsey@gmail.com>
2018-02-28 16:28:54 -0700

    house components

---
+++ src/components/ravenclaw.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="raven">
+<nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a class="active" href="./gryffindor.html">Gryffindor</a></li>
+      <li><a href="./ravenclaw.html">Ravenclaw</a></li>
+      <li><a href="./hufflepuff.html">Hufflepuff</a></li>
+      <li><a href="./syltherin.html">Syltherin</a></li>
+    </ul>
+  </nav>
+
+    <h1>Welcome to the GRYFFINDOR house!</h1>
+    <p1>"You might belong in Gryffindor, Where dwell the brave at heart, Their daring, nerve, and chivalry Set Gryffindors apart" --The Sorting Hat</p1>
+
+</div>
+</template>
---
+++ src/components/slytherin.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="slyth">
+<nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="./gryffindor.html">Gryffindor</a></li>
+      <li><a href="./ravenclaw.html">Ravenclaw</a></li>
+      <li><a href="./hufflepuff.html">Hufflepuff</a></li>
+      <li><a class="active" href="./syltherin.html">Syltherin</a></li>
+    </ul>
+  </nav>
+
+   <h1>Welcome to the SLYTHERIN house!!</h1>
+   <p1>"Or perhaps in Slytherin, You'll make your real friends, Those cunning folk use any means, To achieve their ends." --The Sorting Hat</p1>
+
+</div>
+</template>
